c45d5edc8b7701a24eb7eee490515a58ae2e1792
Shahriar <shahriarfahim20@gmail.com>
2021-06-13 17:06:32 +0600

    php

--- Lab 3/Problem5.php
+++ Lab 3/Problem5.php
@@ -1,17 +1,18 @@
 <?php 
-  $backColor="green"; 
-  $fontColor="red"; 
-  $imageWidth="80px"; $imageHeight="80px";
-?>
+$backColor="green"; 
+$fontColor="red"; 
+$imageWidth="80px";
+ $imageHeight="80px";?>
 <html>
 <body>
-<table>
-<tr><td>
-   <img src="https://en.wikipedia.org/wiki/American_International_University-Bangladesh#/media/File:AIUB_whole_logo.png" width-"imageWidth" height="imageHeight" /> </td>
-<td><h2>American International University-<span style="background-color:yellow; color:cyan"> Bangladesh </h2>
-  </span>
- </td>
- </tr>
- </table>
- </body>
+<table><tr>
+<td>
+<img src="aiub-logo.jpg" width="imageWidth" height="imageHeight">  </td>
+<td><h2>American International University-
+<span style="background-color:backColor; color:fontColor"> 
+Bangladesh </h2></span>
+</td>
+</tr>
+</table> 
+</body>
 </html>
