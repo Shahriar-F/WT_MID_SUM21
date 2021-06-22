8d65c0bc2eb4a032630fd639baa11ad654aa53db
Shahriar <shahriarfahim20@gmail.com>
2021-06-22 22:23:46 +0600

    project-mid

---
+++ Project/Admin.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+
+<html>
+ <head></head>
+  <body>
+       <h1> Welcome, Admin</h1>
+   <form>
+    <fieldset>
+	 <table>
+	  <tr>
+	    <td><a Blood Stock href="blood stock.php">Blood Stock</a> <br>
+		    <a href="Add Member.php">Add Member</a> <br>
+			<a href="Remove Member.php">Remove member</a> <br>
+			<a href="Member Information.php">Member Information</a></td> 
+		
+	  </tr>
+	 
+	 </table>
+	</fieldset>
+   </form>
+  </body>
+</html>
